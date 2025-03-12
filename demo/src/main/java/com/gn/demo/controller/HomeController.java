005cafb1ae5702aefc9ea8f5f837bc59185696b7
4559jacky <4559jacky@gmail.com>
2025-03-12 16:46:21 +0900

    [jji] spring boot 학습 commit

---
+++ demo/src/main/java/com/gn/demo/controller/HomeController.java
@@ -0,0 +1,37 @@
+package com.gn.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.gn.demo.vo.Member;
+
+@Controller
+public class HomeController {
+
+	@GetMapping({"","/"})
+	public ModelAndView home() {
+		// src/main/resources/templates/home.html
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("home");
+		mav.addObject("age",1);
+		return mav;
+	}
+	
+	@GetMapping("/test")
+	public String test(Model model) {
+		// 첫번째 방법.
+		// request.setAttribute("name","김철수");
+		model.addAttribute("name", "김철수");
+		return "test";
+	}
+	
+	@GetMapping("/bye")
+	public String bye(Model model) {
+		Member mem = new Member("홍길동",50);
+		model.addAttribute("member",mem);
+		return "/bye";
+	}
+	
+}
