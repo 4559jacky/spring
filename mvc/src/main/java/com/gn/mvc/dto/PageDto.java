c639efb6517218940b7a42a9bef00ec8dea0a296
4559jacky <4559jacky@gmail.com>
2025-03-17 16:03:24 +0900

    [jji] 게시글 페이징처리 기능 구현 commit

---
+++ mvc/src/main/java/com/gn/mvc/dto/PageDto.java
@@ -0,0 +1,54 @@
+package com.gn.mvc.dto;
+
+import groovy.transform.ToString;
+import groovy.transform.builder.Builder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class PageDto {
+	// 한 페이지에 보이는 데이터 개수
+	private int numPerPage = 5;
+	
+	// 현재 페이지
+	private int nowPage;
+	
+	// 페이징바
+	private int pageBarSize = 5;
+	private int pageBarStart;
+	private int pageBarEnd;
+	
+	// 이전, 다음 여부
+	private boolean prev = true;
+	private boolean next = true;
+	
+	public void setNowPage(int nowPage) {
+		this.nowPage = nowPage;
+	}
+	
+	// 전체 페이지
+	private int totalPage;
+	
+	public void setTotalPage(int totalPage) {
+		this.totalPage = totalPage;
+		calcPaging();
+	}
+	
+	private void calcPaging() {
+		// 페이징바 시작 번호 계산
+		pageBarStart = ((nowPage-1)/pageBarSize)*pageBarSize + 1;
+		// 페이징바 끝 번호 계산
+		pageBarEnd = pageBarStart + pageBarSize - 1;
+		if(pageBarEnd > totalPage) pageBarEnd = totalPage;
+		// 이전,이후 버튼이 보이는지 여부
+		if(pageBarStart == 1) prev = false;
+		if(pageBarEnd == totalPage) next = false;
+	}
+}
