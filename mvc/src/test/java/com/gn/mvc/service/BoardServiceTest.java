12ef118840a5bf89b7310951b4476c8153b70675
4559jacky <4559jacky@gmail.com>
2025-03-18 10:26:46 +0900

    [jji] 테스트코드 JUnit 학습 commit

---
+++ mvc/src/test/java/com/gn/mvc/service/BoardServiceTest.java
@@ -0,0 +1,42 @@
+package com.gn.mvc.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.gn.mvc.entity.Board;
+
+@SpringBootTest
+class BoardServiceTest {
+	
+	@Autowired
+	private BoardService service;
+
+	@Test
+	void testSelectBoardOne_success() {
+		// 1. 예상 데이터
+		Long id = 62L;
+		Board expected = Board.builder().boardTitle("이거 6개인데").build();
+		// 2. 실제 데이터
+		Board real = service.selectBoardOne(id);
+		// 3. 비교 및 검증
+		assertEquals(expected.getBoardTitle(), real.getBoardTitle());
+		
+	}
+	
+	@Test
+	void testSelectBoardOne_fail() {
+		// 1. 예상 데이터
+		Long id = 192L;
+		Board expected = null;
+		// 2. 실제 데이터
+		Board real = service.selectBoardOne(id);
+		// 3. 비교 및 검증
+		assertEquals(expected, real);
+	}
+	
+	
+
+}
