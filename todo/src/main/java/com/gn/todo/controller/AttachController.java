d82efc19374e4617cb3e3d84ba4dc39395e4b90a
4559jacky <4559jacky@gmail.com>
2025-04-02 11:09:34 +0900

    [jji] ci/cd test1

--- todo/src/main/java/com/gn/todo/controller/AttachController.java
+++ todo/src/main/java/com/gn/todo/controller/AttachController.java
@@ -63,6 +63,7 @@ public class AttachController {
 	
 	@GetMapping("/download/{id}")
 	public ResponseEntity<Object> fileDownlod(@PathVariable("id") Long id) {
+		System.out.println("ci/cd test1");
 		try {
 			Attach fileData = attachService.selectAttachOne(id);
 			
